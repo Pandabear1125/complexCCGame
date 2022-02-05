32b4aba82d4ef7ddbca68c9953fa8a1ec6fb72fb
Pandabear1125 <Jvsdestroyer25@gmail.com>
2022-02-05 15:58:29 -0700

    update to inv, acesses items.lua nicely

--- crawler/inv.lua
+++ crawler/inv.lua
@@ -1,17 +1,5 @@
 local selectX, selectY = 1, 1
-
-function getInput()
-   local event, key = os.pullEvent('key')
-   if key == 208 then -- down arrow
-      selectY = selectY + 1
-   elseif key == 200 then -- up arrow 
-      selectY = selectY - 1
-   elseif key == 203 then -- left arrow 
-      selectX = selectX - 1
-   elseif key == 205 then -- right arrow
-      selectX = selectX + 1
-   end 
-end 
+local selectedInv = nil
 
 Inven = {
    x = 0, 
@@ -41,8 +29,42 @@ function Inven:new(t)
    return t 
 end 
 
+local function moveSelectSlot(dx, dy)
+   selectX = selectX + dx
+   selectY = selectY + dy
+   if selectX < 1 then 
+      selectX = 1
+   elseif selectX > selectedInv.width then 
+      selectX = selectedInv.width 
+   end 
+   
+   if selectY < 1 then 
+      selectY = 1
+   elseif selectY > selectedInv.height then 
+      selectY = selectedInv.height 
+   end
+end 
+
+function getInput()
+   local event, key = os.pullEvent('key')
+   if key == 208 then -- down arrow
+      moveSelectSlot(0, 1)
+   elseif key == 200 then -- up arrow 
+      moveSelectSlot(0, -1)
+   elseif key == 203 then -- left arrow 
+      moveSelectSlot(-1, 0)
+   elseif key == 205 then -- right arrow
+      moveSelectSlot(1, 0)
+   end 
+end 
+
+
+function Inven:select()
+   selectedInv = self 
+end 
+
 function Inven:setItem(x, y, itemTable)
-   self.hash[x][y] = itemTable
+   self.hash[y][x] = itemTable
 end 
 
 function Inven:highlightSlot(mon, x, y, ox, oy)
@@ -60,9 +82,24 @@ function Inven:draw(mon, ox, oy)
    local ox, oy = ox or 0, oy or 0
    for i = 1, self.height do 
       for j = 1, self.width do 
-         mon.setCursorPos((i+ox) + (self.buffer+self.slotWidth)*(i-1), (j+oy) + (self.buffer+self.slotHeight)*(j-1))
-         mon.write(self.hash[i][j].value)
+         mon.setCursorPos((i+ox) + (self.buffer+self.slotWidth)*(i-1)+1, (j+oy) + (self.buffer+self.slotHeight)*(j-1)+1)
+         if self.hash[j][i].value ~= '' then 
+            mon.write(items.List[self.hash[i][j].value].name)
+         end             
       end 
    end 
    self:highlightSlot(mon, selectX, selectY, ox, oy)
+   local value = self.hash[selectY][selectX].value
+   mon.setCursorPos(1, 1)
+   mon.write(selectX..' '..selectY)
+   if value ~= '' then
+      mon.setCursorPos(40, 1)
+      mon.write(items.List[value].name)
+      mon.setCursorPos(40, 2)
+      mon.write(items.List[value].desc)
+      mon.setCursorPos(40, 4)
+      mon.write("ATK: "..items.List[value].atk)
+      mon.setCursorPos(40, 5)
+      mon.write("BLK: "..items.List[value].blk)
+   end 
 end 
