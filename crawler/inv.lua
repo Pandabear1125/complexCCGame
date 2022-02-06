97f8d63182714c61b172860d87a43d36884475be
Pandabear1125 <Jvsdestroyer25@gmail.com>
2022-02-06 16:08:46 -0700

    added moving items around and swaping them

--- crawler/inv.lua
+++ crawler/inv.lua
@@ -1,5 +1,6 @@
 local selectX, selectY = 1, 1
 local selectedInv = nil
+local selectedSlot = nil
 
 Inven = {
    x = 0, 
@@ -15,18 +16,39 @@ function Inven:new(t)
    local t = t or {}
    setmetatable(t, self)
    self.__index = self 
-
+   
    t.hash = {}
    for i = 1, t.height do 
       t.hash[i] = {}
       for j = 1, t.width do 
          t.hash[i][j] = {
-            id = ''
          }
       end 
    end 
-
+   
    return t 
+end 
+
+local function pickUpSlot()
+   if selectedSlot then 
+      if selectedInv.hash[selectY][selectX].id then 
+         local oldSlot = {}
+         oldSlot.id = selectedInv.hash[selectY][selectX].id
+         oldSlot.level = selectedInv.hash[selectY][selectX].level
+         selectedInv:setItem(selectX, selectY, selectedSlot)
+         selectedSlot = oldSlot
+      else 
+         selectedInv:setItem(selectX, selectY, selectedSlot)
+         selectedSlot = nil
+      end 
+   else 
+      if selectedInv.hash[selectY][selectX].id then 
+         selectedSlot = {}
+         selectedSlot.id = selectedInv.hash[selectY][selectX].id
+         selectedSlot.level = selectedInv.hash[selectY][selectX].level
+         selectedInv:clearSlot(selectX, selectY)
+      end 
+   end 
 end 
 
 local function moveSelectSlot(dx, dy)
@@ -55,9 +77,14 @@ function getInput()
       moveSelectSlot(-1, 0)
    elseif key == 205 then -- right arrow
       moveSelectSlot(1, 0)
+   elseif key == 57 then -- space 
+      pickUpSlot()
    end 
 end 
 
+function Inven:clearSlot(x, y)
+   self.hash[y][x] = {}
+end 
 
 function Inven:select()
    selectedInv = self 
@@ -82,8 +109,8 @@ function Inven:draw(mon, ox, oy)
    local ox, oy = ox or 0, oy or 0
    for i = 1, self.height do 
       for j = 1, self.width do 
-         mon.setCursorPos((i+ox) + (self.buffer+self.slotWidth)*(i-1)+1, (j+oy) + (self.buffer+self.slotHeight)*(j-1)+1)
-         if self.hash[i][j].id ~= '' then 
+         mon.setCursorPos((j+ox) + (self.buffer+self.slotWidth)*(j-1)+1, (i+oy) + (self.buffer+self.slotHeight)*(i-1)+1)
+         if self.hash[i][j].id then 
             mon.write(items.equip.list[self.hash[i][j].id].name)
          end             
       end 
@@ -92,9 +119,9 @@ function Inven:draw(mon, ox, oy)
    local id = self.hash[selectY][selectX].id
    local level = self.hash[selectY][selectX].level
    mon.setCursorPos(1, 1)
-   mon.write(selectX..' '..selectY)
+   mon.write(selectX..' '..selectY..' '..tostring(selectedSlot))
 
-   if id ~= '' then
+   if id then
       mon.setCursorPos(40, 1)
       mon.write(items.equip.list[id].name)
       mon.setCursorPos(40, 2)
